884f6104362357739046133e4fd32b1938a12dde
MaryB16 <buda.maria89@gmail.com>
2018-03-21 13:21:10 +0200

    feat: Card shuffle function

--- Memory-Game/js/memoryGame.js
+++ Memory-Game/js/memoryGame.js
@@ -1,1 +1,24 @@
-﻿
+﻿let cardElements = document.querySelectorAll('.card');
+cardElements.forEach(function (elem) {
+    elem.addEventListener('click', function () {
+        console.log('I clicked!')
+        console.log(elem)
+        console.log("I clicked what I just logged above")
+       
+    });
+});
+
+function shuffleCards(cardList) {
+    let cardPosition = cardList.length;
+    while (0 !== cardPosition) {
+        let randomCardPosition = Math.floor(Math.random() * cardPosition);
+        cardPosition -= 1;
+        let temporaryCard = cardList[cardPosition].innerHTML;
+        cardList[cardPosition].innerHTML = cardList[randomCardPosition].innerHTML;
+        cardList[randomCardPosition].innerHTML = temporaryCard;
+    }
+
+    return cardList;
+}
+
+shuffleCards(cardElements)
