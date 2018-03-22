2586026b2b89e44490a668be8c802c9f3d724716
MaryB16 <buda.maria89@gmail.com>
2018-03-22 13:36:44 +0200

    feat:Add card match basic function

--- Memory-Game/js/memoryGame.js
+++ Memory-Game/js/memoryGame.js
@@ -1,14 +1,8 @@
-﻿let cardElements = document.querySelectorAll('.card');
-cardElements.forEach(function (elem) {
-    elem.addEventListener('click', function () {
-        console.log('I clicked!')
-        console.log(elem)
-        console.log("I clicked what I just logged above")
-       
-    });
-});
+﻿const cardElements = document.querySelectorAll('.card');
 
-function shuffleCards(cardList) {
+//When the game starts, this function makes sure the cards are 'shuffled'( the cards have a random pattern on the grid)
+
+const shuffleCards= function shuffleCards(cardList) {
     let cardPosition = cardList.length;
     while (0 !== cardPosition) {
         let randomCardPosition = Math.floor(Math.random() * cardPosition);
@@ -20,5 +14,75 @@ function shuffleCards(cardList) {
 
     return cardList;
 }
-
 shuffleCards(cardElements)
+
+
+//This function is called when the card is clicked
+
+const flipp = function flipp(card) {
+     card.classList.add('clicked')
+}
+
+/*
+Alternative way of adding event listeners
+cardElements.forEach(function (card) {
+    card.addEventListener("click", onCardClicked)
+})
+
+function onCardClicked(event) {
+    //do all the card stuff
+}
+*/
+
+let openCard = null;
+let flipping = false
+cardElements.forEach(function (card) {
+
+    card.addEventListener('click', function () {
+        console.log('I clicked! ' + flipping)
+        // we want to check if any card is fliiping, if it's fliiping don't do anything
+        if (flipping == false) {
+            flipp(card);
+
+
+
+            if (openCard == null) {
+                openCard = card;
+            }
+            else {
+                if (card.isEqualNode(openCard)) {
+                    console.log("the two cards are the same, well done");
+                }
+                else {
+                    flipping = true;
+                    console.log("haha try again");
+
+                    setTimeout(() => {
+                        openCard.classList.remove('clicked');
+                        openCard = null;
+                        flipping = false;
+                    }, 500);
+                    // openCard.classList.remove('clicked');
+                    setTimeout(() => card.classList.remove('clicked'), 500);
+                }
+
+
+            }
+        }
+    });
+});
+
+
+
+
+/*
+//add cards to a list
+        cardList.push(card);
+        console.log(cardList);
+        if (cardList.length == 2) {
+            if (cardList[0].isEqualNode(cardList[1])) {
+                console.log('the two cards are the same, well done');
+            }
+            else console.log("haha try again");
+        }
+*/
